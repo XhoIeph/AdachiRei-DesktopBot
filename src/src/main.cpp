46124dff5b421a3da5b1a39c58f0e32a43049cd2
XhoIeph <3425934757@qq.com>
2026-05-14 20:58:25 +0800

    Auto sync src/main.cpp from Astrbot

--- src/src/main.cpp
+++ src/src/main.cpp
@@ -18,9 +18,9 @@
 #include <U8g2lib.h>
 
 // ====== 用户配置 ======
-const char* WIFI_SSID   = "YOUR_SSID";
-const char* WIFI_PASS   = "YOUR_PASS";
-const char* MQTT_BROKER = "broker.emqx.io";
+const char* WIFI_SSID   = "你好";
+const char* WIFI_PASS   = "789247847";
+const char* MQTT_BROKER = "47.110.153.97";
 const int   MQTT_PORT   = 1883;
 const char* MQTT_TOPIC_SUB = "astrbot/esp32/control";
 const char* MQTT_TOPIC_PUB = "astrbot/esp32/status";
